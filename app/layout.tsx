1e4e3665155b54f176854a6e97d67df587f147b9
pokuwagata <pokuwagata@gmail.com>
2023-04-09 22:26:13 +0900

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,13 +33,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-gray-800 text-white">
-        <div className="mx-auto max-w-[960px] px-4">
-          <header className="my-5">
-            <h1 className="text-xl font-bold">
-              <Link href="/">yn2011&apos;s blog</Link>
-            </h1>
-          </header>
-          {children}
+        <div className="mx-auto max-w-[960px] px-4 flex flex-col h-screen">
+          <div className="mb-10 flex-1">
+            <header className="my-5">
+              <h1 className="text-xl font-bold">
+                <Link href="/">yn2011&apos;s blog</Link>
+              </h1>
+            </header>
+            {children}
+          </div>
+          <footer className="bg-gray-700 mx-[calc(50%_-_50vw)] px-[calc(50vw_-_50%)] pt-10 pb-5">
+            <nav className="mb-5">
+              <ul>
+                <li className="mb-3">
+                  <p className="mb-1">
+                    <Link href="/" className="underline text-lg ">
+                      yn2011&apos;s blog
+                    </Link>
+                  </p>
+                  <p className="text-gray-400 text-sm">
+                    yn2011 の技術ブログです。
+                  </p>
+                </li>
+                <li>
+                  <Link href="/about" className="underline">
+                    about
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <p className="text-gray-400 text-xs text-center">© 2023 yn2011</p>
+          </footer>
         </div>
       </body>
     </html>
