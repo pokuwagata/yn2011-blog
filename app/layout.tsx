cc7b8d62caea465bbcca402c31b61220a08fc78d
pokuwagata <pokuwagata@gmail.com>
2023-05-06 21:17:49 +0900

    fix vertical scroll bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,38 +32,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className="bg-gray-800 text-white">
-        <div className="mx-auto flex min-h-screen max-w-[960px] flex-col px-4">
-          <div className="mb-10">
-            <header className="mb-9 mt-3">
-              <h1 className="text-xl font-bold">
-                <Link href="/">yn2011&apos;s blog</Link>
-              </h1>
-            </header>
-            {children}
-          </div>
-          <footer className="mx-[calc(50%_-_50vw)] mt-auto bg-gray-700 px-[calc(50vw_-_50%)] pb-5 pt-10">
-            <nav className="mb-5">
-              <ul>
-                <li className="mb-3">
-                  <p className="mb-1">
-                    <Link href="/" className="text-lg underline ">
-                      yn2011&apos;s blog
+      <body className="bg-gray-800 text-white lg:overflow-x-hidden">
+        <div className="mx-auto max-w-[960px] ">
+          <div className="flex min-h-screen flex-col px-4 lg:px-0">
+            <div className="mb-10">
+              <header className="mb-9 mt-3">
+                <h1 className="text-xl font-bold">
+                  <Link href="/">yn2011&apos;s blog</Link>
+                </h1>
+              </header>
+              {children}
+            </div>
+            <footer className="mx-[calc(50%_-_50vw)] mt-auto bg-gray-700 px-[calc(50vw_-_50%)] pb-5 pt-10">
+              <nav className="mb-5">
+                <ul>
+                  <li className="mb-3">
+                    <p className="mb-1">
+                      <Link href="/" className="text-lg underline ">
+                        yn2011&apos;s blog
+                      </Link>
+                    </p>
+                    <p className="text-sm text-gray-400">
+                      yn2011 の技術ブログです。
+                    </p>
+                  </li>
+                  <li>
+                    <Link href="/about" className="underline">
+                      about
                     </Link>
-                  </p>
-                  <p className="text-sm text-gray-400">
-                    yn2011 の技術ブログです。
-                  </p>
-                </li>
-                <li>
-                  <Link href="/about" className="underline">
-                    about
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            <p className="text-center text-xs text-gray-400">© 2023 yn2011</p>
-          </footer>
+                  </li>
+                </ul>
+              </nav>
+              <p className="text-center text-xs text-gray-400">© 2023 yn2011</p>
+            </footer>
+          </div>
         </div>
       </body>
     </html>
