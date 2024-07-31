6154ebc727f0490c862e93f530c1c5c9aad78f78
pokuwagata <pokuwagata@gmail.com>
2024-07-31 10:16:08 +0900

    fix: inline code style

--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -47,7 +47,7 @@ export const MDXCustomComponents = {
   ),
   code: (props: React.ComponentProps<"code">) => (
     <code
-      className="rounded bg-gray-700 text-[0.8em] text-pink-400"
+      className="rounded bg-gray-700  p-1 align-bottom text-[0.85em] text-pink-300"
       {...props}
     />
   ),
