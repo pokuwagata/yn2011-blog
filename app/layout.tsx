0d4cef14f834033ffb912301f4a0d32855386fb8
pokuwagata <pokuwagata@gmail.com>
2025-01-23 17:07:39 +0900

    expand space and font size

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className="overflow-x-hidden overflow-y-scroll bg-gray-800 text-white">
-        <div className="mx-auto max-w-[960px] ">
+        <div className="mx-auto max-w-[760px] ">
           <div className="flex min-h-screen flex-col px-4 lg:px-0">
             <div className="mb-10">
               <header className="mb-9 mt-3">
--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -4,21 +4,21 @@ export const MDXCustomComponents = {
   h1: (props: React.ComponentProps<"h1">) => (
     <h1
       // className="mx-[calc(50%_-_50vw)] mb-6 mt-auto bg-gray-700 px-[calc(50vw_-_50%)] py-9 text-xl font-semibold leading-relaxed"
-      className="mb-6 mt-3 border-b-2 border-gray-700 pb-6 text-xl font-semibold leading-relaxed"
+      className="mb-6 mt-3 border-b-2 border-gray-700 pb-6 text-3xl font-semibold leading-relaxed"
       {...props}
     />
   ),
   h2: (props: React.ComponentProps<"h2">) => (
-    <h2 className="mb-3 mt-9 text-xl font-semibold" {...props} />
+    <h2 className="mb-[1.5rem] mt-[3.5rem] text-2xl font-semibold" {...props} />
   ),
   h3: (props: React.ComponentProps<"h3">) => (
-    <h3 className="mb-3 mt-6 text-base font-semibold" {...props} />
+    <h3 className="mb-[1.5rem] mt-[2.5rem] text-xl font-semibold" {...props} />
   ),
   p: (props: React.ComponentProps<"p">) => (
-    <p className="mb-3 text-base leading-7" {...props} />
+    <p className="mb-[1rem] text-base leading-8" {...props} />
   ),
   ul: (props: React.ComponentProps<"ul">) => (
-    <ul className="mb-3 list-disc pl-6" {...props} />
+    <ul className="mb-[1rem] list-disc pl-6" {...props} />
   ),
   ol: (props: React.ComponentProps<"ol">) => (
     <ol className="list-decimal pl-6" {...props} />
